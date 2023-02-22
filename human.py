94854f7069f8c8ba2df7980b684f3fe2bfd00929
Kyle Blacker <coachblacker@gmail.com>
2023-02-22 12:16:50 -0700

    feat: added random answer to repetitive bad user_inputs

--- human.py
+++ human.py
@@ -1,4 +1,5 @@
 from player import Player
+import random
 
 class Human(Player):
     def __init__(self, name):
@@ -7,6 +8,7 @@ class Human(Player):
         
     def shoot(self):
         # If we can let's make this 1-5
+        print("")
         print("Select 0 for Rock")
         print("Select 1 for Paper")
         print("Select 2 for Scissors")
@@ -14,11 +16,12 @@ class Human(Player):
         print("Select 4 Spock")
         print("")
         user_input=False
-        while user_input == False:
+        counter = 0
+        while user_input == False and counter < 6:
             shoot_input=input("Which option do you want to choose? ")
             if shoot_input.isnumeric():
                 choose_shoot = int(shoot_input)
-                if choose_shoot <5:
+                if choose_shoot <5 and counter < 5:
                     if choose_shoot==0:
                         print(f"{self.human_player} chooses Rock.")
                         return("Rock")
@@ -35,7 +38,12 @@ class Human(Player):
                         print(f"{self.human_player} chooses Spock.")
                         return ("Spock")
                     user_input=True
+                elif counter > 4:
+                    print(f"{self.human_player} has been assigned {random.choice(self.shoot_list)}!")
+                    return (random.choice(self.shoot_list))
                 else:
                     print('invalid range please choose between 0 and 4! ')
+                    counter += 1
             else:
                 print('You did not enter a valid input! ')
+                counter += 1
